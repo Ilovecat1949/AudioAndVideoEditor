0ec3a342e5c5aa873ab918325bfce6d22899b0ba
ilovecat1949 <1431834899@qq.com>
2025-08-02 11:39:50 +0800

    第一次构建好page

--- app/src/main/java/com/example/audioandvideoeditor/entity/TaskInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.audioandvideoeditor.entity
-
-data class TaskInfo(
-    val int_arr:ArrayList<Int>,
-    val long_arr:ArrayList<Long> ,
-    val str_arr:ArrayList<String>,
-    val float_arr:ArrayList<Float>
-)
--- app/src/main/java/com/example/audioandvideoeditor/viewmodel/UserCenterViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.audioandvideoeditor.viewmodel
-
-import androidx.lifecycle.ViewModel
-
-class UserCenterViewModel : ViewModel() {
-}
